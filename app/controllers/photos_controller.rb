9e6b53453b9571dae7572d9b23597b607d9055a4
Adam Daigian <adam.daigian@gmail.com>
2013-07-10 14:50:47 -0700

    pushing repo to github

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,68 @@
+class PhotosController < ApplicationController
+  def index
+    @photos = Photo.all
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @photo }
+    end
+  end
+
+  def new
+    @photo = Photo.new
+
+    rrespond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @photo }
+    end
+  end
+
+  def create
+    @photo = Photo.new(params[:photo])
+
+    respond_to do |format|
+      if @photo.save
+        format.html { redirect_to @photo, notice: 'photo was successfully created.' }
+        format.json { render json: @photo, status: :created, location: @photo }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @photo.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+    @photo = Photo.find(params[:id])
+  end
+
+  def update
+    @photo = Photo.find(params[:id])
+
+    respond_to do |format|
+      if @photo.update_attributes(params[:photo])
+        format.html { redirect_to @photo, notice: 'photo was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @photo.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @photo = Photo.find(params[:id])
+    @photo.destroy
+
+    respond_to do |format|
+      format.html { redirect_to photos_url }
+      format.json { head :no_content }
+    end
+  end
+
+end
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlickrLab::Application.config.secret_token = '0b39752d86370c89489aae56a00f4258d17b26fd9031aa47eea3cd47853600acb36ff7782dd5cd8d5d8c00f674daf64d7530ba72cb026734c56e374f775c66bb'
